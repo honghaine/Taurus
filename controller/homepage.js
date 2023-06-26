703a98afd76cc3aa43a7cc7d87fcca441372bac9
honghaine <honghai.sg99@gmail.com>
2023-06-26 16:29:11 +0700

    Connect to Postgres

--- controller/homepage.js
+++ controller/homepage.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require('../model/User')
 
-router.get('/', (req, res) => {
-    res.render('home_page');
+router.get("/home", async (req, res) => {
+  const users = await User.findAll();
+  console.log(users.every(user => user instanceof User)); // true
+  console.log("All users:", JSON.stringify(users));
+  res.render("home_page");
 });
 
-router.get('/ourteam', (req, res) => {
-    res.render('our_team');
+router.get("/ourteam", (req, res) => {
+  res.render("our_team");
 });
 
 module.exports = router;
-
-
--- utils/PoolConnection.js
+++ utils/PoolConnection.js
@@ -1,16 +1,19 @@
-const {Sequelize} = require('sequelize');
-const env = require('../env/properties.json');
+const { Sequelize } = require("sequelize");
+const env = require("../env/properties.json");
 
 
-const sequelize = new Sequelize(env.url);
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  schema: env.schema, // Change this to your desired schema
+});
 
 const testDbConnection = async () => {
-    try {
-      await sequelize.authenticate();
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
-  };
-  
-  module.exports = {sq: sequelize, testDbConnection};
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
+module.exports = { sq: sequelize, testDbConnection };
