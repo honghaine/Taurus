feb2e2d3a1f160a1e034cb5294e56db3d1c8dc86
honghaine <honghai.sg99@gmail.com>
2023-06-28 14:21:51 +0700

    Add guild image

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-const express = require('express');
-const path = require('path');
-const db = require('./utils/PoolConnection');
+const express = require("express");
+const path = require("path");
+const db = require("./utils/PoolConnection");
 
 const app = express();
 
-const homepageController = require('./controller/homepage');
+const homepageController = require("./controller/homepage");
 
 // Set up the view engine and views directory
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 
 // Serve static files
-app.use(express.static('public'));
-app.use('/styles', express.static(path.join(__dirname, 'public/styles')));
-app.use('/script', express.static(path.join(__dirname, 'public/script')));
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.log('Error:', err);
-  res.status(500).render('500');
-});
+app.use(express.static("public"));
+app.use("/styles", express.static(path.join(__dirname, "public/styles")));
+app.use("/script", express.static(path.join(__dirname, "public/script")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(homepageController);
 
+app.use((err, req, res, next) => {
+  res.status(404).render("404");
+});
+
 // Establish database connection and start the server
 (async () => {
-    try {
-      db.testDbConnection;
-      console.log('Connection has been established successfully.');
-      app.listen(3000, () => {
-        console.log('Server listening on port 3000');
-      });
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  })();
+  try {
+    db.testDbConnection;
+    console.log("Connection has been established successfully.");
+    app.listen(3000, () => {
+      console.log("Server listening on port 3000");
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+// Error handling middleware
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
--- controller/homepage.js
+++ controller/homepage.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const User = require('../model/User')
 
 router.get("/home", async (req, res) => {
-
   res.render("home_page");
 });
 
-router.get("/ourteam", async (req, res) => {
+router.get("/onlylooker", async (req, res) => {
   const users = await User.findAll();
   res.render("our_team", {contents: users});
 });
