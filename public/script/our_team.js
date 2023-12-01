da199cc226c66c81c989f5afd8da386c28f7918d
honghaine <111057775+hai-to-CLV@users.noreply.github.com>
2023-12-01 13:54:37 +0700

    Edit chart for members

--- public/script/our_team.js
+++ public/script/our_team.js
@@ -1,6 +1,5 @@
-function seeChart(cardElement) {
-  // Extract the data for the chart from the card or any other source
-  const newData = {
+document.addEventListener("DOMContentLoaded", function () {
+  const initialData = {
     labels: [
       'Eating',
       'Drinking',
@@ -9,62 +8,64 @@ function seeChart(cardElement) {
       'Coding',
       'Cycling',
       'Running'
-    ], // Update with new labels
+    ],
     datasets: [{
-      label: 'My Updated Dataset',
-      data: [80, 59, 90, 81, 90, 55, 40], // Update with new data
+      label: 'My First Dataset',
+      data: [65, 59, 90, 81, 90, 55, 40],
       fill: true,
-      backgroundColor: 'rgba(0, 128, 0, 0.2)', // Change colors as needed
-      borderColor: 'rgb(0, 128, 0)',
-      pointBackgroundColor: 'rgb(0, 128, 0)',
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgb(0, 128, 0)'
+      pointHoverBorderColor: 'rgb(255, 99, 132)'
     }]
   };
 
-  // Update the chart's data
-  radarChart.data = newData;
-
-  // Update the chart
-  radarChart.update();
-}
-
-const initialData = {
-  labels: [
-    'Eating',
-    'Drinking',
-    'Sleeping',
-    'Designing',
-    'Coding',
-    'Cycling',
-    'Running'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 90, 81, 90, 55, 40],
-    fill: true,
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgb(255, 99, 132)',
-    pointBackgroundColor: 'rgb(255, 99, 132)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(255, 99, 132)'
-  }]
-};
-
-const config = {
-  type: 'radar',
-  data: initialData,
-  options: {
-    elements: {
-      line: {
-        borderWidth: 3
+  const config = {
+    type: 'radar',
+    data: initialData,
+    options: {
+      elements: {
+        line: {
+          borderWidth: 3
+        }
       }
     }
-  }
-};
+  };
 
-const ctx = document.getElementById('radarChart').getContext('2d');
-const radarChart = new Chart(ctx, config);
+  const cards = document.querySelectorAll('.card');
 
+  cards.forEach((card, index) => {
+    const canvas = card.querySelector('canvas');
+    const radarChart = new Chart(canvas, config);
+
+    card.addEventListener('mouseover', function () {
+      const newData = {
+        labels: [
+          'Eating',
+          'Drinking',
+          'Sleeping',
+          'Designing',
+          'Coding',
+          'Cycling',
+          'Running'
+        ],
+        datasets: [{
+          label: `Dataset ${index + 1}`, // Unique label for each dataset
+          data: [Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100],
+          fill: true,
+          backgroundColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.2)`,
+          borderColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+          pointBackgroundColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+        }]
+      };
+
+      radarChart.data = newData;
+      radarChart.update();
+    });
+  });
+});
